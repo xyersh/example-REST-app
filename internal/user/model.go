101e87a50bb8a52333ca75913c343622ad0e483e
Alex Yersh <xyersh@gmail.com>
2025-09-10 17:55:57 +0300

    new file:   internal/user/db/mongodb.go - сюда реализация функций для работы с юзерами в mongoDB 	modified:   internal/user/model.go 	modified:   internal/user/storage.go

--- internal/user/model.go
+++ internal/user/model.go
@@ -1,1 +1,14 @@
 package user
+
+type User struct {
+	ID           string `json:"id" bson:"_id,omitempty"`
+	username     string `json:"username" bson:"username"`
+	passwordHash string `json:"-" bson:"password"`
+	Email        string `json:"email" bson:"email"`
+}
+
+type CreateUserDTO struct {
+	username string `json:"username"`
+	password string `json:"passsword"`
+	Email    string `json:"email"`
+}
--- internal/user/storage.go
+++ internal/user/storage.go
@@ -1,1 +1,18 @@
 package user
+
+import "context"
+
+type Storage interface {
+
+	//Создание пользователя
+	Create(ctx *context.Context, user User) (string, error)
+
+	// поиск пользователя по ID
+	FindOne(ctx *context.Context, id string) (User, error)
+
+	// изменение пользователя
+	Update(ctx *context.Context, user User) error
+
+	// удаление пользователя
+	Delete(ctx *context.Context, id string) error
+}
